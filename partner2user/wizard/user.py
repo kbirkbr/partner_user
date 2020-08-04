32852233cc04ede5873b83e7c9b856420a203db1
Aitor Rosell Torralba <aitor.rosell.torralba@gmail.com>
2020-08-04 16:17:11 +0200

    multi group users

--- partner2user/wizard/user.py
+++ partner2user/wizard/user.py
@@ -2,12 +2,13 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-
+import re
 
 class CreateUser(models.TransientModel):
     _name = 'partner.user'
     _description = 'Create login from partner'
 
+    groups_id = fields.Many2many('res.groups',column1='user_datas_id',column2="group_id",string='Groups')
     @api.model
     def default_get(self, fields):
         res = super(CreateUser, self).default_get(fields)
@@ -15,7 +16,7 @@ class CreateUser(models.TransientModel):
         for aid in self._context.get('active_ids'):
             partner_id = self.env['res.partner'].browse(aid)
             if not partner_id.user_id:
-                vals.append((0,0,{'partner_id':partner_id.id,'login':partner_id.email}))
+                vals.append((0,0,{'partner_id':partner_id.id,'login':partner_id.email or re.sub('[^A-Za-z0-9-_]+', '', partner_id.name.replace(" ", "_")).lower()}))
         res['user_data'] = vals
         return res
     user_data = fields.One2many('user.datas','rel_id')
@@ -27,7 +28,7 @@ class CreateUser(models.TransientModel):
                 vals = {
                         'partner_id':da.partner_id.id,
                         'login':da.login,
-                        'groups_id':da.groups_id
+                        'groups_id':data.groups_id
                         }
                 user = self.env['res.users'].create(vals)
                 da.partner_id.user_id = user.id
@@ -37,4 +38,3 @@ class CreateUserData(models.TransientModel):
     rel_id = fields.Many2one('partner.user')
     partner_id = fields.Many2one('res.partner',string='Partner')
     login = fields.Char('Login')
-    groups_id = fields.Many2many('res.groups',column1='user_datas_id',column2="group_id",string='Groups')
